02bcce8f4642aed38690cc9ce2e8c918800edc67
Jeniza Chala <52945554+jenizachala@users.noreply.github.com>
2021-06-29 13:55:23 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,4 @@
+#Example Locations for ISSLocator
+
+#Houston Museum of Natural Science / Empire State Building / National Gallery in Singapore
+gradle run --args="29.7216183,-95.3893835 40.7487128,-73.9859724 1.290270,103.851959"
